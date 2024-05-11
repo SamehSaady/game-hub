467fe9a575a618456b0ab90f65b42e37cfba2eeb
Sameh Saady <samehsaady77@gmail.com>
2024-05-12 01:19:46 +0300

    Connect to RAWG API and fetch games

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   // First Row has two Columns: [nav] and [nav].
@@ -19,7 +20,9 @@ function App() {
         <GridItem area="aside">Aside</GridItem>
       </Show>
 
-      <GridItem area="main">Main</GridItem>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
     </Grid>
   );
 }
---
+++ src/services/http-service.ts
@@ -0,0 +1,40 @@
+// In this file, we are craeting a generic HTTP service
+// with (get, post, patch, and delete) requests.
+import apiClient from "./api-client";
+
+interface Entity {
+    id: number;
+}
+
+class HttpService {
+  endPoint: string;
+
+  constructor(endPoint: string) {
+    this.endPoint = endPoint;
+  }
+
+  getAll<T>() {
+    const controller = new AbortController();
+    const request = apiClient.get<T[]>(this.endPoint, {
+        signal: controller.signal,
+      })
+
+      return {request, cancel: () => controller.abort()}
+    }
+
+    delete(id: number) {
+        return apiClient.delete(this.endPoint + "/" + id);
+    }
+
+    create<T>(entity: T) {
+        return apiClient.post(this.endPoint, entity);
+    }
+
+    update<T extends Entity>(entity: T) {
+        return apiClient.patch(this.endPoint + "/" + entity.id, entity)
+    }
+}
+
+const create = (endPoint: string) => new HttpService(endPoint);
+
+export default create;
