7795d209fd13d382ade6505d6099e24f18bafa6d
Sameh Saady <samehsaady77@gmail.com>
2024-05-11 22:42:34 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <div></div>;
+  // First Row has two Columns: [nav] and [nav].
+  // Second Row has two Columns: [aside] and [main].
+  return (
+    <Grid
+      templateAreas={{
+        base: '"nav" "main"', // Mobile layout
+        lg: '"nav nav" "aside main"', // Desktop layout (>= 992px)
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
