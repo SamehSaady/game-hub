647222aec5720a92b59e23c509f109d2983f900d
Sameh Saady <samehsaady77@gmail.com>
2024-05-13 00:14:54 +0300

    Fixed image path issue

--- src/components/navBarComps/NavBar.tsx
+++ src/components/navBarComps/NavBar.tsx
@@ -1,6 +1,6 @@
 import { HStack, Image } from "@chakra-ui/react";
 // import logo from "../assets/icons8-game-controller-windows-11-color-32.png";
-import logo from "../assets/icons8-game-controller-windows-11-color-96.png";
+import logo from "../../assets/icons8-game-controller-windows-11-color-96.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
