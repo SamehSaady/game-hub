9e46c7c8cd048021f9e58306a17ed2a6e26cd25e
Sameh Saady <samehsaady77@gmail.com>
2024-05-13 23:15:59 +0300

    Handling games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,12 @@
+// import noImage from "../assets/No-Image-Placeholder-1.svg";
+import noImage from "../assets/No-Image-Placeholder-1-Gray_BG (600x400).png";
+
+// Retrieves the passed image cropped to 600x400 from the RAWG API.
+// If url is [null] (a game doesn't have an image), a [noImage placeholder] image will be returned instead.
 // It's better to pass [width] & [height] as parameters,
 // but the back-end (RAWG API) supports cropping with this dimension only.
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return ""; // If url is [null] (a game doesn't have an image).
+  if (!url) return noImage;
 
   const target = "media/";
   const index = url.indexOf(target) + target.length;
