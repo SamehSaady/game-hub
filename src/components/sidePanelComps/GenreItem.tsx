3e7b6a78479e51644ecf22825037e84f17b89d08
Sameh Saady <samehsaady77@gmail.com>
2024-05-14 18:04:39 +0300

    Optimizing the look & feel of the genres list

--- src/components/sidePanelComps/GenreItem.tsx
+++ src/components/sidePanelComps/GenreItem.tsx
@@ -1,5 +1,5 @@
 import { Genre } from "../../services/genre-service";
-import { Button, HStack, Image } from "@chakra-ui/react";
+import { Box, Button, HStack, Image } from "@chakra-ui/react";
 import getCroppedImageUrl from "../../services/image-url";
 
 interface Props {
@@ -10,24 +10,27 @@ interface Props {
 
 const GenreItem = ({ genre, onSelectGenre, highlight }: Props) => {
   return (
-    <HStack>
+    <Button
+      width="100%"
+      fontWeight={highlight ? "bold" : "normal"}
+      fontSize="lg"
+      whiteSpace="wrap"
+      textAlign="left"
+      variant="ghost"
+      display="flex" // To make children align horizontally.
+      alignItems="center" // Align children vertically to center.
+      onClick={onSelectGenre}
+    >
       <Image
         boxSize="32px"
         borderRadius={8}
         objectFit="cover"
+        marginRight={3}
         src={getCroppedImageUrl(genre.image_background)}
       />
-      <Button
-        fontWeight={highlight ? "bold" : "normal"}
-        fontSize="lg"
-        whiteSpace="wrap"
-        textAlign="left"
-        variant="link"
-        onClick={onSelectGenre}
-      >
-        {genre.name}
-      </Button>
-    </HStack>
+      {/* // Added a wrapper <Box> to control text alignment */}
+      <Box flex="1"> {genre.name}</Box>
+    </Button>
   );
 };
 
